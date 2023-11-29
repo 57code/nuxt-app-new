1b42d5cb4a7235da776c5a47fd055ffd46152fe3
村长 <33857444+57code@users.noreply.github.com>
2023-11-29 22:39:16 +0800

    配置和meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "村长的技术空间",
+      meta: [
+        { name: "description", content: "专注于前端技术分享" },
+        { name: "keywords", content: "nuxt,vue,ts,frontend" },
+      ]
+    },
+  },
   devtools: { enabled: true },
   css: [
     'assets/global.css'
---
+++ pages/config.vue
@@ -0,0 +1,19 @@
+<template>
+  <div :class="{ dark: appConfig.theme.dark }">
+    <p
+      class="bg-blue-300 dark:bg-slate-600 dark:text-slate-200"
+      @click="appConfig.title = 'hello'"
+    >
+      appConfig: {{ appConfig.title }}
+    </p>
+
+    <label>
+      dark mode: {{ appConfig.theme.dark }}
+      <UToggle v-model="appConfig.theme.dark" />
+    </label>
+  </div>
+</template>
+
+<script setup lang="ts">
+const appConfig = useAppConfig();
+</script>
