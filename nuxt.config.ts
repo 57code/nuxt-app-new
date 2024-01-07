9fbfea8f96e6e2c83f24ba754a70bc8e1fc96efc
村长 <33857444+57code@users.noreply.github.com>
2024-01-07 18:33:06 +0800

    icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  ui: {
+    icons: ['carbon']
+  },
+  imports: {
+    dirs: ['store'],
+  },
   modules: ['@nuxt/ui', [
     '@pinia/nuxt',
     {
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
-const {value} = storeToRefs(useCounter)
+const { value: count } = storeToRefs(useCounter())
 </script>
 
 <template>
   <h1>Index Page</h1>
   <p>
     <UButton>Nuxt3 in Action</UButton>
+  </p>
+  <p>
+    <UIcon name="i-carbon-time" />
+    counter: {{ count }}
   </p>
 </template>
